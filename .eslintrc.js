eba625e82ef9b3924ac247bc363c4cc7725bd08d
tamm <tmmoond8@gmail.com>
2021-07-05 22:26:43 +0900

    feat: eslint, prettierrc 추가

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["airbnb/hooks"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true,
+    },
+    extraFileExtensions: ["js", "jsx"],
+  },
+};
